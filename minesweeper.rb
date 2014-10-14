74e487e2af8ac574c72e691bd03c4543fe0d361a
App Academy Student <student@appacademy.io>
2014-10-13 18:07:59 -0700

    Added serialization

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,5 +1,6 @@
 require './tile'
 require './board'
+require 'yaml'
 class Game 
   attr_accessor :board 
   def initialize
@@ -7,11 +8,17 @@ class Game
   end
   
   def run
+    start_time = Time.now
+    
     until win? || lose?
       @board.display
       puts "Please choose a square: (put input in [x,y] format)"
       @user_input = gets.chomp
-      if @user_input.start_with?("f")
+      if @user_input == "save"
+        File.open('save.txt', 'w') { |file| file.write(@board.to_yaml) }
+      elsif @user_input == "load"
+       File.open('save.txt', 'r') {|file| @board = YAML.load(file) }
+      elsif @user_input.start_with?("f")
         @user_input = @user_input[1..-1].split(',').map { |num| num.to_i}
         @board.board[@user_input[0]][@user_input[1]].flag
       else
@@ -23,11 +30,15 @@ class Game
     
     if win?
       puts "You win!"
+      end_time = Time.now
+      time_taken = (end_time - start_time).to_i
+      puts "Your time is #{time_taken} seconds"
     elsif lose?
       puts "You lose!"
     end
     
     puts "Game over!"
+    
   end
   
   def win? 
@@ -42,6 +53,11 @@ end
 game = Game.new
 game.run
 
+
+#gem colorize
+# $stdin.getch
+# io/console
+#system("clear")
 
 
 
